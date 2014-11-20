326ac4318e5d40c6d38972f030f805cf19ef7aa1
TC94615 <TCLee0117@gmail.com>
2014-11-20 18:52:41 +0800

    beeeeaaaan

---
+++ TableViewTest/UIViewController+Bean.h
@@ -0,0 +1,9 @@
+//
+// Created by 李道政 on 14/11/20.
+// Copyright (c) 2014 李道政. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIViewController(Bean)
+@end
---
+++ TableViewTest/UIViewController+Bean.m
@@ -0,0 +1,10 @@
+//
+// Created by 李道政 on 14/11/20.
+// Copyright (c) 2014 李道政. All rights reserved.
+//
+
+#import "UIViewController+Bean.h"
+
+
+@implementation UIViewController(Bean)
+@end
